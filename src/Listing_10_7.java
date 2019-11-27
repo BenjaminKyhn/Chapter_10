1c7656f9d28e8cb3a57942d7d46f64cfc8db4843
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-27 10:31:08 +0100

    Listing 10.7 added

---
+++ src/Listing_10_7.java
@@ -0,0 +1,12 @@
+public class Listing_10_7 {
+    public static void main(String[] args) {
+        StackOfIntegers stack = new StackOfIntegers();
+
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+
+        while (!stack.empty())
+            System.out.println(stack.pop());
+    }
+}
